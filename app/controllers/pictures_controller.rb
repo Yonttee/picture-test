282b4536440dc5327aaf50caeef816d31d35f704
Carriane Linzer <carrianelinzer@icloud.com>
2019-11-25 15:47:36 +0000

    activated link

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -7,6 +7,18 @@ class PicturesController < ApplicationController
     @picture = Picture.new
   end
 
+
+  def create
+    Picture.create(picture_params)
+    redirect_to root_path
+  end
+
+  private
+
+  def picture_params
+    params.require(:picture).permit(:image, :description)
+  end
+
   def about
    
   end
